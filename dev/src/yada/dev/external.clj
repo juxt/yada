a3f9cbb061637030cb2befc25b706c3d35963a19
Malcolm Sparks <malcolm@juxt.pro>
2015-04-27 22:45:10 +0100

    Add comment

--- dev/src/yada/dev/external.clj
+++ dev/src/yada/dev/external.clj
@@ -1,3 +1,5 @@
+;; Copyright © 2015, JUXT LTD.
+
 (ns yada.dev.external
   (:require
    [bidi.bidi :refer (RouteProvider tag)]
@@ -16,7 +18,11 @@
         (for [[_ h] (:examples user-guide)]
           [(get-path h) (make-handler h)]))]]]))
 
-(defn new-external-resources [& {:as opts}]
+(defn new-external-resources
+  "External resources are those that run on a different port, such that
+  they are considered to be from a different origin, from a CORS
+  perspective. This allows testing of CORS functionality."
+  [& {:as opts}]
   (->
    (->> opts
         (merge {})
