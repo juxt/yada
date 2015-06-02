c129bbbe2612c0022d125f29318ea77369b7ce62
Malcolm Sparks <malcolm@juxt.pro>
2015-06-02 17:16:38 +0100

    given improvements

--- test/yada/test/util.clj
+++ test/yada/test/util.clj
@@ -29,14 +29,20 @@
   `(do
      ~@(for [[a b c] (partition 3 body)]
         (case b
+          ;; Equals?
           := `(is (= ((as-test-function ~a) ~v) ~c))
           :!= `(is (not= ((as-test-function ~a) ~v) ~c))
+          ;; Schema checks
           :- `(is (nil? (s/check ~c ((as-test-function ~a) ~v))))
+          ;; Is?
           :? `(is (~c ((as-test-function ~a) ~v)))
           :!? `(is (not (~c ((as-test-function ~a) ~v))))
+          ;; Matches regex?
           :# `(is (re-matches (re-pattern ~c) ((as-test-function ~a) ~v)))
           :!# `(is (not (re-matches (re-pattern ~c) ((as-test-function ~a) ~v))))
-          :> `(is (set/subset? (set ~c) (set ((as-test-function ~a) ~v))))
-          :!> `(is (not (set/subset? (set ~c) (set ((as-test-function ~a) ~v)))))
-          :< `(is (set/superset? (set ~c) (set ((as-test-function ~a) ~v))))
-          :!< `(is (not (set/superset? (set ~c) (set ((as-test-function ~a) ~v)))))))))
+          ;; Is superset?
+          :> `(is (set/superset? (set ((as-test-function ~a) ~v)) (set ~c)))
+          :!> `(is (not (set/superset? (set ((as-test-function ~a) ~v)) (set ~c))))
+          ;; Is subset?
+          :< `(is (set/subset? (set ((as-test-function ~a) ~v)) (set ~c)))
+          :!< `(is (not (set/subset? (set ((as-test-function ~a) ~v)) (set ~c))))))))
