d03d6c3af8f5486c638336c47f58ee51608a06e5
Malcolm Sparks <malcolm@juxt.pro>
2015-06-02 08:06:17 +0100

    OPTIONS stub

---
+++ test/yada/options_test.clj
@@ -0,0 +1,27 @@
+;; Copyright © 2015, JUXT LTD.
+
+(ns yada.options-test
+  (:require
+   [clojure.java.io :as io]
+   [clojure.test :refer :all]
+   [ring.mock.request :refer [request]]
+   [yada.core :refer [yada]]
+   [yada.test.util :refer (given)]))
+
+(deftest options-test []
+  (testing "Server capabilities"
+    (let [resource {}
+          handler (yada resource)
+          request (request :options "*")
+          response @(handler request)]
+      (given response
+        :status 200)))
+
+  (testing "Resource capabilities"
+    (let [resource {}
+          handler (yada resource)
+          request (request :options "/")
+          response @(handler request)]
+      (given response
+        :status 200
+        :headers {"accept" "(TODO)"}))))
