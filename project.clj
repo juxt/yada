eb589cab1cba19d453881714ce5c8070658a2de5
Malcolm Sparks <malcolm@juxt.pro>
2015-06-17 13:01:47 +0100

    Bumped version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;; Copyright © 2015, JUXT LTD.
 
-(defproject yada "0.4.3"
+(defproject yada "0.5.0-SNAPSHOT"
   :description "A library for Clojure web APIs"
   :url "http://github.com/juxt/yada"
   :license {:name "The MIT License"
