d421bdd24b612dbea5649b31e6bd11515a338d8b
Malcolm Sparks <malcolm@juxt.pro>
2015-08-20 06:02:49 +0100

    Add comment about url-resource

--- src/yada/resources/url_resource.clj
+++ src/yada/resources/url_resource.clj
@@ -25,6 +25,14 @@
     [{:media-type #{(ext-mime-type (.getPath u))}
       :charset charset/platform-charsets}])
 
+  ;; TODO: This is wrong. First, an InputStreamReader does not coerce the
+  ;; encoding, simply converts a byte stream to a character stream and is
+  ;; told the encoding OF the byte stream for it to do so. Second, it is
+  ;; reasonable to guess the media-type, but the charset must be provided
+  ;; by the options - this means that there must be more sophisticated way
+  ;; of (representations) accessing the options itself. We must pass
+  ;; options to representations. This requires a change to the protocol.
+
   Get
   (GET [u ctx]
     (if (= (get-in ctx [:response :representation :media-type :type]) "text")
