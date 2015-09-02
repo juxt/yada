975880589df5bd00e276d60ea23f6f6ff34fff8b
Malcolm Sparks <malcolm@juxt.pro>
2015-09-02 14:12:27 +0100

    CORS demo index now uses template

--- dev/src/yada/dev/cors_demo.clj
+++ dev/src/yada/dev/cors_demo.clj
@@ -5,7 +5,7 @@
    [clojure.java.io :as io]
    [bidi.bidi :refer (RouteProvider)]
    [bidi.ring :refer (files resources-maybe)]
-   [com.stuartsierra.component :refer [using Lifecycle]]
+   [com.stuartsierra.component :refer [using]]
    [hiccup.core :refer (html)]
    [modular.component.co-dependency :refer (co-using)]
    [modular.template :as template :refer (render-template)]
@@ -15,30 +15,20 @@
 
 (defn index [{:keys [*router templater]}]
   (yada
-   (fn [ctx]
-     ;; TODO: Replace with template resource
-     (render-template
-      templater
-      "templates/page.html.mustache"
-      {:content
-       (html
-        [:div.container
-         [:h2 "CORS demo"]
-         ])}))
-   {:id ::index
-    :representations [{:media-type "text/html"
-                       :charset "utf-8"}]}))
+   (new-template-resource "templates/page.html"
+    {:content
+     (html
+      [:div.container
+       [:h2 "CORS demo"]
+       ])})
+   {:id ::index}))
 
-(defrecord CorsDemo [*router templater template]
-  Lifecycle
-  (start [component] (assoc component :template (new-template-resource "cors/ajax.js2" {:origin "ABC"})))
-  (stop [component] component)
-
+(defrecord CorsDemo [*router templater]
   RouteProvider
   (routes [component]
     ["/"
      [["index.html" (index component)]
-      ["ajax.js" (yada template)]
+      ["ajax.js" (yada (new-template-resource "cors/ajax.js" {:origin "ABC"}))]
       ]]))
 
 (defn new-cors-demo [& {:as opts}]
--- dev/src/yada/dev/template.clj
+++ dev/src/yada/dev/template.clj
@@ -2,17 +2,18 @@
 
 (ns yada.dev.template
   (:require
+   [clj-time.core :refer [now]]
+   [clojure.java.io :as io]
    [clojure.string :as str]
    [clojure.tools.logging :refer :all]
    [com.stuartsierra.component :refer [Lifecycle]]
-   [clj-time.core :refer [now]]
-   [ring.util.mime-type :refer (ext-mime-type)]
-   [yada.protocols :as p]
-   [clojure.java.io :as io]
    [modular.template :refer (render-template)]
+   [ring.util.mime-type :refer (ext-mime-type)]
    [stencil.core :as stencil]
    [stencil.loader :as loader]
+   [yada.charset :as charset]
    [yada.methods :as m]
+   [yada.protocols :as p]
    [yada.util :refer [as-file]]))
 
 (defrecord TemplateResource [template-name model *template]
@@ -27,11 +28,24 @@
       {:allowed-methods #{:get}
        :representations [{:media-type (or (ext-mime-type template-name)
                                           "application/octet-stream"
-                                          )}]
+                                          )
+                          ;; Since stencil produces strings, we can
+                          ;; specify the encoding and yada's
+                          ;; representation logic takes care of the
+                          ;; encoding.
+                          :charset charset/default-platform-charset}]
+
+       ;; We can work out last-modified, iff model is not a function
+       ;; then last-modified date is now
+
+       ::create-time (.getMillis (now))
        ::resource resource
        ::file (as-file resource)}))
   (resource-properties
-    [_ ctx])
+    [_ ctx]
+    (when-not (fn? model)
+      {:last-modified (max (-> ctx :resource-properties ::file .lastModified)
+                           (-> ctx :resource-properties ::create-time))}))
 
   m/Get
   (GET [_ ctx]
@@ -52,7 +66,3 @@
 
 (defn new-template-resource [template model]
   (->TemplateResource template model (atom nil)))
-
-
-
-(.lastModified (io/file "README.md"))
