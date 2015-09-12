781a568ecbc446c5a5486dc54a40019b6125ee8f
Malcolm Sparks <malcolm@juxt.pro>
2015-09-12 15:47:09 +0100

    Journal browser

--- src/yada/resources/journal_browser.clj
+++ src/yada/resources/journal_browser.clj
@@ -7,29 +7,40 @@
    [yada.charset :as charset]
    [yada.journal :as journal]
    [yada.methods :refer [Get]]
+   [yada.media-type :refer [media-type]]
    [yada.protocols :as p]
    [yada.yada :as yada]))
+
+(def access-control
+  {:allow-origin "*"
+   :expose-headers ["Server" "Date" "Content-Length" "Access-Control-Allow-Origin"]})
 
 (defn index [idx]
   (yada/resource
    (fn [ctx]
-     (html
-      [:body
-       [:h1 "Error index"]
-       [:ul
-        (for [[k v] idx]
-          [:li [:a {:href (journal/path-for :entry :id k)} k]]
-          )]
-       ]))
+     (case (-> ctx :response :representation :media-type media-type)
+       "text/html"
+       (html
+        [:body
+         [:h1 "Error index"]
+         [:ul
+          (for [[k v] idx]
+            [:li [:a {:href (journal/path-for :entry :id k)} k]]
+            )]
+         ])
+       "application/edn"
+       (into {} (for [[k v] idx]
+                  [k (:chain (get idx k))]
+                  ))))
    {:allowed-methods #{:get}
-    :media-type "text/html"
+    :access-control access-control
+    :media-type #{"text/html" "application/edn" "application/edn;pretty=true"}
     :charset charset/platform-charsets}))
 
 (defn entry [e]
   (yada/resource e {:media-type "text/html"
-                    :charset charset/platform-charsets}))
-
-
+                    :charset charset/platform-charsets
+                    :access-control access-control}))
 
 ;; TODO: Improve rendering
 
