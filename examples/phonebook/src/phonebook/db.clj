4526cd12053ff77cd2199330c38306ed2af18be9
Malcolm Sparks <malcolm@juxt.pro>
2015-09-30 19:16:36 +0100

    Initiate phonebook nextval properly

--- examples/phonebook/src/phonebook/db.clj
+++ examples/phonebook/src/phonebook/db.clj
@@ -3,12 +3,17 @@
 (ns phonebook.db)
 
 (defn create-db [entries]
-  {:phonebook (ref entries) :next-entry (ref (inc (count entries)))})
+  {:phonebook (ref entries)
+   :next-entry (ref (inc (apply max (keys entries))))})
 
 (defn add-entry
-  "Add a new entry to the database"
+  "Add a new entry to the database. Returns the id of the newly added
+  entry."
   [db entry]
   (dosync
+   ;; Why use 2 refs when one atom would do? It comes down to being able
+   ;; to return nextval from this function. While this is possible to do
+   ;; with an atom, its feels less elegant.
    (let [nextval @(:next-entry db)]
      (alter (:phonebook db) conj [nextval entry])
      (alter (:next-entry db) inc)
