7e15ba54a1625f38e51f0358b890217bcd0d5535
Malcolm Sparks <malcolm@juxt.pro>
2015-10-13 20:29:20 +0100

    Multipart

--- src/yada/multipart.clj
+++ src/yada/multipart.clj
@@ -23,8 +23,155 @@
 ;; turn. If a given buffer does not end in a boundary, we store the
 ;; partial data until a boundary is found in a subsequent buffer.
 
+(defn- seam
+  "Find the position of a potential boundary straddling across a pair of byte-arrays."
+  [previous input boundary-size index]
+  (let [b (byte-array (* 2 boundary-size))]
+    (System/arraycopy (:bytes previous) (- (count (:bytes previous)) boundary-size) b 0 boundary-size)
+    (System/arraycopy (:bytes input) 0 b boundary-size boundary-size)
+    (first (match index b))))
 
-(defn create-matcher [boundary]
-  (let [index (bm-index (b/to-byte-array boundary))]
-    (fn [source]
-      (match index (b/to-byte-array source)))))
+(defn ->multipart-events
+  "Returns a stateful transducer that processes a series of buffer
+  events and combines them into a stream of multipart events. Parts will
+  be delivered either in entirety, or as a series of
+  {partial,continuation*,completion} events. The first part of a partial
+  will be at least one buffer size large, so any text headers can be
+  processed. Boundaries that straddle buffers are detected and handled."
+  [boundary-size index]
+  (fn [rf]
+    (let [mem (volatile! nil)]
+      (fn
+        ([] (rf))
+        ([result] (rf result))
+        ([result input]
+         (let [previous @mem]
+           (case (:type input)
+             :part (rf result [input])
+
+             :leader (do
+                       (vreset! mem input)
+                       (rf result []))
+
+             ;; :remainder terminates a part, the question is whether
+             ;; there's a boundary straddling the previous input and this
+             ;; one
+             :remainder
+             (do
+               (vreset! mem nil)
+
+               (rf result
+                   (if-let [seam (seam previous input boundary-size index)]
+                     (case (:type previous)
+                       :leader
+                       [{:type :part
+                         :from (:from previous)
+                         :to (+ (count (:bytes previous)) (- boundary-size) seam)
+                         :pos (+ (:from previous) boundary-size)
+                         :first-bytes (take 2 (drop (+ (:from previous) boundary-size) (:bytes previous)))
+                         :chunk (:chunk previous)}
+                        {:type :part
+                         :from (+ (count (:bytes previous)) (- boundary-size) seam)
+                         :to (:to input)
+                         :pos seam
+                         :first-bytes (take 2 (drop seam (:bytes input)))
+                         :chunks [(:chunk previous) (:chunk input)]
+                         }]
+
+                       :block
+                       (throw (ex-info (format "TODO: %s" (:type previous)) {})))
+
+                     ;; No seam
+                     (case (:type previous)
+                       ;; leader->remainder (no seam)
+                       :leader [{:type :part
+                                 :from (:from previous)
+                                 :to (:to input)
+                                 :chunks [(:chunk previous) (:chunk input)]}]
+                       ;; block->remainder (no seam)
+                       :block [{:type :completion
+                                ;; Whenever block is in mem, only the last boundary size bytes are unserved
+                                :from (- (count (:bytes previous)) boundary-size)
+                                :to (:to input)
+                                :chunks [(:chunk previous) (:chunk input)]}]))))
+
+             :block
+             (do
+               (vreset! mem input)
+
+               (rf result
+                   (if-let [seam (seam previous input boundary-size index)]
+                     (case (:type previous)
+                       :leader (throw (ex-info "TODO: leader->block (with seam)" {}))
+                       :block (throw (ex-info "TODO: block->block (with seam)" {}))
+                       )
+                     (case (:type previous)
+                       ;; leader->block (without seam)
+                       :leader [{:type :partial
+                                 :from (:from previous)
+                                 :pos (+ (:from previous) boundary-size)
+                                 :to (- (count (:bytes input)) boundary-size)
+                                 :first-bytes (take 2 (drop  (+ (:from previous) boundary-size) (:bytes previous)))
+                                 :chunks [(:chunk previous) (:chunk input)]
+                                 }]
+                       ;; block->block (without seam)
+                       :block [{:type :continuation
+                                :from (- (count (:bytes previous)) boundary-size)
+                                :to (- (count (:bytes input)) boundary-size)
+                                :chunks [(:chunk previous) (:chunk input)]}]))))
+
+             ;; otherwise
+             (rf result []))))))))
+
+;; [ ] Put algorithm below into a function.
+;; [ ] Write tests that cause seams between continuations, testing all cases (without the need for fuzz testing)
+;; [ ] Storing already served bytes in :block mem is wasteful, avoid this
+;; [ ] Don't use drop to get first two bytes, use get (is there a quick indexed byte-array get operation?)
+;; [ ] Parse to CRLFCRLF and strip out 7-bit headers (Content-Disposition)
+;; [ ] Integrate with form handler, where each field can be given a particular handler (String capture, etc.)
+
+
+(defn- split-buf
+  [n index boundary-size buf]
+  (let [bytes (b/to-byte-array buf)
+        offsets (match index bytes)]
+    (concat
+     [{:chunk-index n
+       :chunk-size (count bytes)}]
+     (when (pos? (or (first offsets) 0))
+       [{:type :remainder
+         :to (first offsets)
+         :bytes bytes
+         :chunk n}])
+     (for [[from to] (partition 2 1 offsets)
+           :let [pos (+ from boundary-size)]]
+       {:type :part
+        :from from :to to :pos pos
+        :first-bytes (take 2 (drop pos bytes))
+        :bytes bytes
+        :chunk n})
+     (if (last offsets)
+       [{:type :leader
+         :from (last offsets)
+         :bytes bytes
+         :chunk n}]
+       [{:type :block
+         :bytes bytes
+         :chunk n}])
+     [{:chunk :end}])))
+
+(defn ->splitter
+  "Returns a stateful transducer that parses a stream of buffers,
+  splitting each element in the stream into parts separated by a
+  boundary. The resulting stream of byte arrays can be fed into
+  ->multipart-events."
+  [boundary-size index]
+  (fn [rf]
+    (let [seqn (volatile! 0)]
+      (fn
+        ([] (rf))
+        ([result] (rf result))
+        ([result buf]
+         (do
+           (let [n (vswap! seqn inc)]
+             (rf result (split-buf n index boundary-size buf)))))))))
--- test/yada/multipart_test.clj
+++ test/yada/multipart_test.clj
@@ -2,13 +2,66 @@
 
 (ns yada.multipart-test
   (:require
-   [clj-index.core :refer [bm-index match]]
    [byte-streams :as b]
-   [juxt.iota :refer [given]]
+   [clj-index.core :refer [bm-index match]]
+   [clojure.string :as str]
    [clojure.test :refer :all]
    [clojure.java.io :as io]
+   [manifold.stream :as s]
+   [juxt.iota :refer [given]]
    [yada.media-type :as mt]
-   [yada.multipart :refer [create-matcher]]))
+   [yada.multipart :refer [->splitter ->multipart-events]]))
+
+(defn generate-boundary []
+  (str
+   "--"
+   (apply str (map char (take 30
+                              (shuffle
+                               (concat
+                                (range (int \A) (inc (int \Z)))
+                                (range (int \a) (inc (int \z)))
+                                (range (int \0) (inc (int \9))))))))))
+
+(defn generate-body [n]
+  ;; We want to generate an odd number of characters (23), so we don't use Z.
+  (apply str (repeat n (apply str (map char (shuffle (range (int \A) (int \Z))))))))
+
+(defn line [& args]
+  (str (apply str args) "\r\n"))
+
+(defn generate-multipart [boundary parts#]
+  (apply str
+         (apply str
+                (for [n (range parts#)
+                      :let [nm (format "content%d" (inc n))]]
+                  (str
+                   (line boundary)
+                   (line (format "Content-Disposition: form-data; name=\"%s\"" nm))
+                   (line)
+                   (line (generate-body (case n 4 130 16))))))
+         (str boundary "--")
+         ))
+
+(defn to-chunks [s]
+  (b/to-byte-buffers s {:chunk-size 1000}))
+
+(defn filter-by-type [t]
+  (partial sequence (filter #(= (:type %) t))))
+
+(deftest multipart-events-test
+  (let [boundary (generate-boundary)]
+    (let [index (bm-index (b/to-byte-array boundary))
+          source (-> (generate-multipart boundary 10)
+                     to-chunks
+                     s/->source)]
+      (let [s (->> source
+                   (s/transform (->splitter (count boundary) index))
+                   (s/mapcat identity)
+                   (s/transform (->multipart-events (count boundary) index))
+                   (s/mapcat identity))]
+        (given (s/stream->seq s 100)
+          [(filter-by-type :part) count] := 9
+          [(filter-by-type :partial) count] := 1)))))
 
 (deftest parse-content-type-header
   (let [content-type "multipart/form-data; boundary=----WebKitFormBoundaryZ3oJB7WHOBmOjrEi"]
@@ -18,9 +71,78 @@
       :subtype := "form-data"
       [:parameters "boundary"] := "----WebKitFormBoundaryZ3oJB7WHOBmOjrEi")))
 
+(defn as-multipart-byte-buffer
+  "Convert a readable to a byte-buffer, with CRLF line endings."
+  [r]
+  (b/to-byte-buffer
+   (apply str (map #(str % "\r\n")
+                   (line-seq (io/reader r))))))
+
+(defn create-matcher [boundary]
+  (let [index (bm-index (b/to-byte-array boundary))]
+    (fn [source]
+      (match index source))))
+
 (deftest boundary-search-test
   (is
    (=
-    ((create-matcher "----WebKitFormBoundaryZ3oJB7WHOBmOjrEi")
-     (io/input-stream (io/resource "yada/multipart-1")))
-    '(2 97 193 285))))
+    ((create-matcher (str "--" "----WebKitFormBoundaryZ3oJB7WHOBmOjrEi"))
+     (b/to-byte-array (as-multipart-byte-buffer (io/resource "yada/multipart-1"))))
+    '(0 99 199 295))))
+
+(defn break? [bytes pos]
+  (= [(get bytes pos)
+      (get bytes (+ pos 1))
+      (get bytes (+ pos 2))
+      (get bytes (+ pos 3))]
+     (map byte [13 10 13 10])))
+
+(defn scan-for-break [bytes offset stop]
+  (loop [pos offset]
+    (cond
+      (> pos (- stop 4)) nil
+      (break? bytes pos) (+ 4 pos)
+      :otherwise (recur (inc pos)))))
+
+#_(let [charset (java.nio.charset.Charset/forName "US-ASCII")
+      decoder (.newDecoder charset)
+
+      boundary "----WebKitFormBoundaryZ3oJB7WHOBmOjrEi"
+      matcher (create-matcher (str "--" boundary))
+
+      multipart (b/to-byte-array (as-multipart-byte-buffer (io/resource "yada/multipart-1")))
+
+      offsets (matcher multipart)
+      offset-pairs (partition 2 1 offsets)]
+
+  (for [[start end] offset-pairs
+        :let [start (+ start (count boundary) 2)]]
+    (when-let [break (scan-for-break multipart start end)]
+      {:headers (str/split (.trim (String. multipart start (- break start) charset)) #"\r\n")
+       :bytes (let [len (- end break 2)
+                b (byte-array len)]
+            (System/arraycopy multipart break b 0 len)
+            b)}))
+
+  ;; Need to compare last offset to see if this is the last part.  If
+  ;; not, we may still have more buffers to follow. Transfer the bytes
+  ;; out to disk or some storage.
+
+  ;;(b/print-bytes (.. body slice (position 97) (limit 193)))
+  )
+
+(comment
+  charbuf (.decode decoder part)
+  matcher (re-matcher #"\r\n([^\r]\r\n)\r\n" charbuf)
+  groups (when (re-find matcher)
+           (re-groups matcher)))
+
+(re-find (re-matcher #"Content-Disposition" "Content-Disposition: form-data; name=\"firstname\""))
+
+;;{:offsets (0 95 191 283)}
+;;{:offsets (2 97 193 285)}
+
+(comment
+  (.get body b 0 10)
+  #_(b/print-bytes (.get body b 0 10))
+  (b/print-bytes b))
