3942bad1dd036f364a450bb60f8c357fc4db9d23
Malcolm Sparks <malcolm@juxt.pro>
2016-03-07 10:59:57 +0000

    tweaks

--- src/yada/consume.clj
+++ src/yada/consume.clj
@@ -8,7 +8,8 @@
    [manifold.deferred :as d]
    [manifold.stream :as s]))
 
-(defn save-to-file [ctx body-stream f]
+;; Works ok
+#_(defn save-to-file [ctx body-stream f]
   (let [fos (new java.io.FileOutputStream f false)
         fc (.getChannel fos)]
     (d/chain
@@ -17,12 +18,34 @@
 
         (infof "count is %s" (:count ctx))
         (let [niobuf (b/to-byte-buffer buf)]
-          (infof "Writing single buffer" (.capacity niobuf))
+          (infof "Writing single buffer %s" (.capacity niobuf))
           (.write fc niobuf))
         
         (update ctx :count (fnil inc 0)))
       ctx
       (s/batch 100 100 body-stream))
+     (fn [ctx]
+       (.close fc)
+       (.close fos)
+       (assoc-in ctx [:file] f)))))
+
+
+(defn save-to-file [ctx body-stream f]
+  (let [fos (new java.io.FileOutputStream f false)
+        fc (.getChannel fos)]
+    (d/chain
+     (s/reduce
+      (fn [ctx buf]
+        #_(infof "count is %s" (:count ctx))
+        (.retain buf)
+        (let [niobuf (b/to-byte-buffer buf)]
+          #_(infof "Writing single buffer %s, refCnt is %s" (.capacity niobuf) (.refCnt buf))
+          (.write fc niobuf))
+        (.release buf)
+        
+        (update ctx :count (fnil inc 0)))
+      ctx
+      body-stream)
      (fn [ctx]
        (.close fc)
        (.close fos)
