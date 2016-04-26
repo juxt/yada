dcc72a892e2cb44ddcadb2876a1759fcc274eb95
Malcolm Sparks <malcolm@juxt.pro>
2016-04-26 13:14:03 +0100

    Remove test: relativize has been promoted to bidi

--- test/yada/bidi_test.clj
+++ test/yada/bidi_test.clj
@@ -7,25 +7,7 @@
    [bidi.vhosts :refer [vhosts-model make-handler]]
    [clojure.test :refer :all]
    [ring.mock.request :refer [request]]
-   [yada.context :refer [relativize]]
    [yada.yada :refer [uri-for resource]]))
-
-(deftest relativize-test
-  (are [source dest href] (= href (relativize source dest))
-    ;; TODO: Test to itself
-    "" "" nil
-    "/abc/cd/d.html" "/abc/" "../"
-    "/abc/foo.html" "/abc/bar.html" "bar.html"
-    "/abc/foo/a.html" "/abc/bar/b.html" "../bar/b.html"
-    "/abc/foo/a/b" "/abc/bar/b" "../../bar/b"
-    "/abc.html" "/abc.html" "abc.html"
-    "/a/abc.html" "/a/abc.html" "abc.html"
-
-    "/a/" "/a/abc.html" "abc.html"
-    "/a" "/a/abc.html" "a/abc.html"
-
-    "/a/abc.html" "/a/" ""
-    ))
 
 (deftest uri-for-test
   (let [h
