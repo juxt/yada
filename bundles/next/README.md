8d4775dff9f027233d69daf9043322d3c577aa1e
Malcolm Sparks <malcolm@juxt.pro>
2017-01-15 03:02:06 +0000

    Add new bundle: next
    This is intended for migration to clojure.spec. It's currently also the
    intention that this is a temporary bundle and will eventually be rolled
    into yada (default)
    
    It also requires that we have a new aleph-next extension to support
    Clojure 1.9

---
+++ bundles/next/README.md
@@ -0,0 +1,10 @@
+# Yada Next
+
+A new API on yada that replaces Prismatic Schema with clojure.spec,
+and drives yada forward with new features.
+
+## Install
+
+```clojure
+[yada/next "1.2.0"]
+```
---
+++ bundles/next/project.clj
@@ -0,0 +1,20 @@
+;; Copyright © 2014-2017, JUXT LTD.
+
+(def VERSION "1.2.1")
+
+(defproject yada/next VERSION
+
+  :pedantic? :abort
+
+  :dependencies [[yada/aleph-next ~VERSION]
+                 [yada/bidi ~VERSION]
+                 [yada/core ~VERSION]
+                 [yada/json ~VERSION]
+                 [yada/json-html ~VERSION]
+                 [yada/jwt ~VERSION]
+                 [yada/multipart ~VERSION]
+                 [yada/oauth2 ~VERSION]
+                 [yada/transit ~VERSION]
+                 [yada/webjars ~VERSION]]
+
+  :profiles {:test {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]}})
