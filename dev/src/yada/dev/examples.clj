7fd6d95568d68b19f2825b6c478c590046c09244
Malcolm Sparks <malcolm@juxt.pro>
2017-06-10 18:54:35 +0100

    Add example of SSE channel close notification

--- dev/src/yada/dev/examples.clj
+++ dev/src/yada/dev/examples.clj
@@ -1,13 +1,32 @@
 (ns yada.dev.examples
   (:require
    [yada.yada :as yada]
-   [manifold.stream :as ms]))
+   [manifold.stream :as ms]
+   [manifold.deferred :as d]))
 
 (defn routes []
   ["/examples"
    [
-    ["/sse-body" (yada/resource
-             {:methods
-              {:get {:produces "text/event-stream"
-                     :response (fn [ctx] (ms/periodically 400 (fn [] "foo")))}}})]
-    ["/sse-resource" (yada/handler (ms/periodically 400 (fn [] "foo")))]]])
+    ["/sse-resource" (yada/handler (ms/periodically 400 (fn [] "foo")))]
+    ["/sse-body"
+     (yada/resource
+      {:methods
+       {:get {:produces "text/event-stream"
+              :response (fn [ctx]
+                          (let [source (ms/periodically 400 (fn [] "foo"))
+                                sink (ms/stream 10)]
+                            (ms/on-closed
+                             sink
+                             (fn [] (println "closed")))
+                            (ms/connect source sink)
+                            sink
+                            ))}}})]
+    ["/sse-body-with-close"
+     (yada/resource {:methods {:get
+                               {:produces "text/event-stream"
+                                :response (fn [ctx]
+                                            (let [source (ms/periodically 400 (fn [] "foo"))
+                                                  sink (ms/stream 10)]
+                                              (ms/on-closed sink (fn [] (println "closed")))
+                                              (ms/connect source sink)
+                                              sink))}}})]]])
