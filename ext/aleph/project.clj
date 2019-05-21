5d88f88c5afa4f5099452b602b6ba3de67451373
Malcolm Sparks <mal@juxt.pro>
2019-05-21 12:23:47 +0100

    Ready for release 1.3.0-alpha11

--- ext/aleph/project.clj
+++ ext/aleph/project.clj
@@ -1,6 +1,6 @@
 ;; Copyright © 2014-2017, JUXT LTD.
 
-(def VERSION "1.3.0-alpha10")
+(def VERSION "1.3.0-alpha11")
 
 (defproject yada/aleph VERSION
   :description "yada integration with Aleph (and Netty)"
