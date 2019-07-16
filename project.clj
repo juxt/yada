55c275090833345b247ac19461abb0e7f2c2eda8
Malcolm Sparks <mal@juxt.pro>
2019-07-16 17:40:12 +0100

    Release fix to multipart

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;; Copyright © 2014-2017, JUXT LTD.
 
-(def VERSION "1.2.15")
+(def VERSION "1.2.15.1")
 
 (defproject yada/core VERSION
   :description "A powerful Clojure web library, full HTTP, full async"
