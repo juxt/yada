2648c1d2dc2bc1458c6fcfc2aa956b2f85b315eb
Tim Greene <tim.g.greene@gmail.com>
2021-01-22 13:35:06 +0000

    Limit logging of 501 Not Implemented errors
    For public facing websites it's not uncommon to have exploitation suites run
    random strings as methods in small batches of requests. This commit channels
    501s away from errors logs into info logs as we may still want to know that
    unknown methods are being used, typically though it's a part of the normal
    course of operation and don't want it to fill logging with high priority
    messages and potentially fall through into alerting for some users.

--- test/yada/interceptor_test.clj
+++ test/yada/interceptor_test.clj
@@ -1,12 +1,14 @@
 ;; Copyright © 2014-2017, JUXT LTD.
 
 (ns yada.interceptor-test
-  (:require
-   [clojure.test :refer :all :exclude [deftest]]
-   [schema.test :refer [deftest]]
-   [yada.handler
-    :refer
-    [append-interceptor insert-interceptor prepend-interceptor]]))
+  (:require [clojure.test :refer :all :exclude [deftest]]
+            clojure.tools.logging
+            [ring.mock.request :as ring-mock]
+            [schema.test :refer [deftest]]
+            [yada.handler
+             :refer
+             [append-interceptor handler insert-interceptor prepend-interceptor]]
+            [yada.resource :refer [as-resource]]))
 
 (deftest prepend-interceptor-test
   (let [res {:interceptor-chain [:b :c :d :e]}]
@@ -29,3 +31,19 @@
         (= expected (:interceptor-chain f))
       (append-interceptor res :c :c1 :c2) [:b :c :c1 :c2 :d :e]
       (append-interceptor res :z :d1 :d2) [:b :c :d :e])))
+
+(deftest known-method-logging-test
+  (let [logs (atom [])]
+    (with-redefs [clojure.tools.logging/log*
+                  (fn [_ level _ message]
+                    (swap! logs conj [level message]))]
+      (let [resource "Hello World!"
+            h (handler (merge (as-resource resource)
+                              {:produces {:media-type "text/plain"
+                                          :charset "UTF-8"}}))
+            request (ring-mock/request :foo "/")
+            response @(h request)
+            headers (:headers response)]
+
+        (is (= [[:info "Unknown method :foo /"]] @logs))
+        (is (= 501 (:status response)))))))
